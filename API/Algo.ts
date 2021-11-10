8b2e4c9484d4668ef42da0ba4e349a14510df73a
Unknown <yossilan@post.bgu.ac.il>
2021-11-11 00:23:04 +0200

    Ready for testing

--- API/Algo.ts
+++ API/Algo.ts
@@ -35,7 +35,7 @@ function buildGraph(cluster: LocationCluster): DirectedGraph<LocationNode> {
     return graph;
 }
 
-function getOptimalPath(cluster: LocationCluster) {
+function getOptimalPath(cluster: LocationCluster): LocationNode[] {
     const { locations, distances } = cluster;
     const graph = buildGraph(cluster);
 
@@ -44,24 +44,57 @@ function getOptimalPath(cluster: LocationCluster) {
     });
     const restLocations: LocationNode[] = locations.filter(location => location.id != startNode.id);
     const startTime: number = startNode.workingHours.opening + startNode.stayDuration;
-    const optimalPath: string[] = evalOptimalPath(startNode,distances,restLocations, [], startTime);
+    const optimalPath: LocationNode[] = evalOptimalPath(startNode,distances,restLocations, [startNode], startTime);
+
+    return optimalPath;
 }
 
-function evalOptimalPath(node:LocationNode,distances: Distances, restLocations:LocationNode[], optimalPath:string[], currentTime: number): string[] | undefined {
-    const nodeToVisit = restLocations.reduce(function(prev, curr) {
-        const prevDistanceTime = distances[node.id][prev.id];
-        const currDistanceTime = distances[node.id][curr.id];
-        return getVisitWeight(prev, currentTime, prevDistanceTime) < getVisitWeight(curr, currentTime, currDistanceTime) ? prev : curr;
+function evalOptimalPath(currNode:LocationNode,distances: Distances, unvisited:LocationNode[], visited:LocationNode[], currentTime: number): LocationNode[] | null {
+    if(unvisited.length ==0)
+        return visited;
+
+    const nodeToVisit = unvisited.reduce(function(prev, curr) {
+        const prevDistanceTime = distances[currNode.id][prev.id];
+        const currDistanceTime = distances[currNode.id][curr.id];
+        const prevWeight = getVisitWeight(prev, currentTime, prevDistanceTime);
+        const currWeight = getVisitWeight(curr, currentTime, currDistanceTime);
+        if(prevWeight != null && curr == null)
+            return prev;
+        if(prevWeight == null && curr != null)
+            return curr;
+        if(prevWeight == null && curr == null)
+            return null;
+        else{
+            return prevWeight < currWeight ? prev : curr;
+        }
     });
 
-    return path;
+    if(nodeToVisit != null){
+        unvisited = unvisited.filter(node => node.id != nodeToVisit.id);
+        visited.push(nodeToVisit);
+        const { workingHours: { opening, closing}, stayDuration } = nodeToVisit;
+        currentTime = opening >= currentTime + distances[currNode.id][nodeToVisit.id] ?  opening + stayDuration : currentTime + distances[currNode.id][nodeToVisit.id] + stayDuration;
+        //visited = evalOptimalPath(nodeToVisit, distances, unvisited, visited, currentTime); 
+    } else{
+        visited = visited.filter(node => node.id != currNode.id);//pop curr
+        //unvisited.push(currNode);
+        //visited = evalOptimalPath(nodeToVisit, distances, unvisited, visited, currentTime); 
+    }
+    
+    visited = evalOptimalPath(nodeToVisit, distances, unvisited, visited, currentTime); 
 }
 
-function getVisitWeight(destNode: LocationNode, currentTime: number, distanceTime: number): number | undefined {
+function getVisitWeight(destNode: LocationNode, currentTime: number, distanceTime: number): number | null {
+    const { workingHours: { opening: destOpeningTime, closing: destClosingTime}, stayDuration } = destNode;
     const arrivalTime = currentTime + distanceTime;
-    const waitTime = destNode.workingHours.opening - arrivalTime;
+    const waitTime = destOpeningTime - arrivalTime;
     const weight = distanceTime + waitTime;
-    const exitTime = 
-    if()
+    const startActivityTime = waitTime >= 0 ?  destOpeningTime : arrivalTime;
+
+    if(destClosingTime >= startActivityTime + stayDuration)
+        return weight;
+    else{
+        return null;
+    }
 }
 
