cf198f6cc9da80d351604d49e50fd42465184a14
eyal kapon <eyalkapon007@gmail.com>
2021-11-11 02:33:02 +0200

    need to do map and pass with props

--- UI/get-around/src/App.tsx
+++ UI/get-around/src/App.tsx
@@ -1,111 +1,152 @@
-import React, { FC, useState } from 'react';
-import logo from './assets/logo.png';
-import mapPhoto from './assets/map.jpeg';
-import AsyncSelect from 'react-select/async';
-import AwesomeDebouncePromise from 'awesome-debounce-promise';
-import axios from 'axios';
-import { MultiValue } from 'react-select';
-import './App.css';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { Backdrop, Box, createTheme, Fade, Modal, Typography } from '@mui/material';
-import { ThemeProvider } from '@emotion/react';
-import { DateRange, DateRangePicker, RangeKeyDict } from 'react-date-range';
-import { addDays } from 'date-fns';
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
-import { style } from '@mui/system';
-import { MyComponent } from './s';
+import React, { FC, useState } from "react";
+import logo from "./assets/logo.png";
+import mapPhoto from "./assets/map.jpeg";
+import AsyncSelect from "react-select/async";
+import AwesomeDebouncePromise from "awesome-debounce-promise";
+import axios from "axios";
+import { MultiValue } from "react-select";
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import {
+  Backdrop,
+  Box,
+  createTheme,
+  Fade,
+  Modal,
+  Typography,
+} from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import { DateRange, DateRangePicker, RangeKeyDict } from "react-date-range";
+import { addDays } from "date-fns";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { style } from "@mui/system";
+import { MyComponent } from "./s";
 
 interface Option {
-	value: string;
-	label: string;
+  value: string;
+  label: string;
 }
 
 const _loadOptions = async (inputValue: string): Promise<Option[]> => {
-	const options = (await axios.post<Option[]>(`http://localhost:8000/api/places/search`, { value: inputValue })).data;
+  const options = (
+    await axios.post<Option[]>(`http://localhost:8000/api/places/search`, {
+      value: inputValue,
+    })
+  ).data;
 
-	return options;
+  return options;
 };
 
 const theme = createTheme({
-	palette: {
-		primary: {
-			main: '#B01AA7',
-		},
-		secondary: {
-			main: '#FFC74F',
-			contrastText: '#ffffff',
-		},
-	},
+  palette: {
+    primary: {
+      main: "#B01AA7",
+    },
+    secondary: {
+      main: "#FFC74F",
+      contrastText: "#ffffff",
+    },
+  },
 });
 
 const loadOptions = AwesomeDebouncePromise(_loadOptions, 500);
 
 const App: FC = () => {
-	const [places, setPlaces] = useState<MultiValue<Option>>([]);
-	const [numOfDays, setNumOfDays] = useState<number>(1);
-	const [dateRangeOpen, setDateRangeOpen] = useState(false);
-	const [dates, setDate] = useState<RangeKeyDict>({ range1: { startDate: new Date(), endDate: addDays(new Date(), 1) } });
+  const [places, setPlaces] = useState<MultiValue<Option>>([]);
+  const [numOfDays, setNumOfDays] = useState<number>(1);
+  const [dateRangeOpen, setDateRangeOpen] = useState(false);
+  const [mapOpen, setMapOpen] = useState(false);
+  const [daysRoutes,setDaysRoutes] = useState([]);
+  const [dates, setDate] = useState<RangeKeyDict>({
+    range1: { startDate: new Date(), endDate: addDays(new Date(), 1) },
+  });
 
-	console.log(dates);
+  console.log(dates);
 
-	return (
-		<div className='App'>
-			<div className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<img src={mapPhoto} className='Map-logo' alt='logo' />
-			</div>
-			<div className='App-body'>
-				<form>
-					<ThemeProvider theme={theme}>
-						<AsyncSelect
-							isMulti
-							cacheOptions
-							noOptionsMessage={() => 'Loading...'}
-							placeholder='Search For Attractions'
-							loadOptions={loadOptions}
-							className='places-select'
-							onChange={setPlaces}
-							value={places}
-							theme={(theme) => ({ ...theme, colors: { ...theme.colors, primary: '#B01AA7' } })}
-						/>
-						<Button onClick={() => setDateRangeOpen(true)} color='secondary' variant='contained' className='date-picker-button'>
-							Select Dates
-						</Button>
-						<Modal
-							open={dateRangeOpen}
-							onClose={() => setDateRangeOpen(false)}
-							closeAfterTransition
-							BackdropComponent={Backdrop}
-							BackdropProps={{
-								timeout: 500,
-							}}
-						>
-							<Fade in={dateRangeOpen}>
-								<div className='date-range-field-con'>
-									<MyComponent />
-									{/* <DateRange
-										onChange={(newValue) => setDate((currValue) => ({ ...currValue, ...newValue }))}
-										editableDateInputs={true}
-										minDate={new Date()}
-										ranges={[dates.range1]}
-										className='date-range-field'
-									/> */}
-								</div>
-							</Fade>
-						</Modal>
-						<div className='spacer' />
-						<Button variant='contained' className='submit-button'>
-							Press For Magic
-						</Button>
-					</ThemeProvider>
-				</form>
-			</div>
-			<div className='circle first-circle'></div>
-			<div className='circle second-circle'></div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={mapPhoto} className="Map-logo" alt="logo" />
+      </div>
+      <div className="App-body">
+        <form>
+          <ThemeProvider theme={theme}>
+            <AsyncSelect
+              isMulti
+              cacheOptions
+              noOptionsMessage={() => "Loading..."}
+              placeholder="Search For Attractions"
+              loadOptions={loadOptions}
+              className="places-select"
+              onChange={setPlaces}
+              value={places}
+              theme={(theme) => ({
+                ...theme,
+                colors: { ...theme.colors, primary: "#B01AA7" },
+              })}
+            />
+            <Button
+              onClick={() => setDateRangeOpen(true)}
+              color="secondary"
+              variant="contained"
+              className="date-picker-button"
+            >
+              Select Dates
+            </Button>
+            <Modal
+              open={dateRangeOpen}
+              onClose={() => setDateRangeOpen(false)}
+              closeAfterTransition
+              BackdropComponent={Backdrop}
+              BackdropProps={{
+                timeout: 500,
+              }}
+            >
+              <Fade in={dateRangeOpen}>
+                <div className="date-range-field-con">
+                   {
+                    <DateRange
+                      onChange={(newValue) =>
+                        setDate((currValue) => ({ ...currValue, ...newValue }))
+                      }
+                      editableDateInputs={true}
+                      minDate={new Date()}
+                      ranges={[dates.range1]}
+                      className="date-range-field"
+                    />
+                  }
+                </div>
+              </Fade>
+            </Modal>
+            <div className="spacer" />
+
+            <Button variant="contained" onClick ={() => setMapOpen(true)} className="submit-button"></Button>
+            <Modal
+              open={mapOpen}
+              onClose={() => setMapOpen(false)}
+              closeAfterTransition
+              BackdropComponent={Backdrop}
+              BackdropProps={{
+                timeout: 500,
+              }}
+            >
+              <Fade in={mapOpen}>
+                <div className="date-range-field-con">
+                  <MyComponent  />
+
+                </div>
+              </Fade>
+            </Modal>
+          </ThemeProvider>
+        </form>
+      </div>
+      <div className="circle first-circle"></div>
+      <div className="circle second-circle"></div>
+    </div>
+  );
 };
 
 export default App;
--- UI/get-around/src/s.tsx
+++ UI/get-around/src/s.tsx
@@ -1,42 +1,60 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { GoogleMap, DirectionsRenderer } from '@react-google-maps/api';
+import React, { useEffect, useMemo, useState } from "react";
+import { GoogleMap, DirectionsRenderer } from "@react-google-maps/api";
 
 const containerStyle = {
-	width: '400px',
-	height: '400px',
+  width: "400px",
+  height: "400px",
 };
 
 const center = {
-	lat: -3.745,
-	lng: -38.523,
+  lat: -3.745,
+  lng: -38.523,
 };
 
 export const MyComponent = () => {
-	const [origin, setOrigin] = useState({ lat: 40.756795, lng: -73.954298 });
-	const [destination, setDestination] = useState({ lat: 41.756795, lng: -78.954298 });
-	const [result, setResult] = useState<google.maps.DirectionsResult>();
-	const directionsService = useMemo(() => new google.maps.DirectionsService(), []);
+  const [origin, setOrigin] = useState({ lat: 41.756797, lng: -73.954298 });
+  const [destination, setDestination] = useState({
+    lat: 41.756796,
+    lng: -73.954296,
+  });
+  const [waypoints, setWaypoints] = useState<google.maps.DirectionsWaypoint[]>(
+    []
+  );
+  const [result, setResult] = useState<google.maps.DirectionsResult>();
+  const directionsService = useMemo(
+    () => new google.maps.DirectionsService(),
+    []
+  );
 
-	useEffect(() => {
-		directionsService.route(
-			{
-				origin,
-				destination,
-				travelMode: google.maps.TravelMode.TRANSIT,
-			},
-			(response, status) => {
-				console.log(response);
-				console.log(status);
-				response && setResult(response);
-			}
-		);
-	}, [origin, destination]);
 
-	return (
-		<GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-			<DirectionsRenderer directions={result} />
-		</GoogleMap>
-	);
+  useEffect(() => {
+    setWaypoints([
+      ...waypoints,
+      { location: new google.maps.LatLng({ lat: 41.756796, lng: -73.954298 }) },
+    ]);
+    directionsService.route(
+      {
+        origin,
+        destination,
+        waypoints,
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (response, status) => {
+        console.log(response);
+        console.log(status);
+        response && setResult(response);
+      }
+    );
+
+  }, [origin, destination]);
+
+  return (
+	  <>	  
+       <GoogleMap  mapContainerStyle={containerStyle} center={center} zoom={10}>
+      <DirectionsRenderer  directions={result} />
+    </GoogleMap>
+	</>
+  );
 };
 
 export default React.memo(MyComponent);
